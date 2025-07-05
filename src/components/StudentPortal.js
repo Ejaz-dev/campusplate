42a9dfd95cb45f9e86a1f5d44177fed4f17e7d36
Ejaz Khan <ejazkhan0123@gmail.com>
2025-07-05 01:39:39 -0230

    bug fix

--- src/components/StudentPortal.js
+++ src/components/StudentPortal.js
@@ -333,7 +333,7 @@ const StudentPortal = ({ setActivePortal }) => {
                 <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4" />
                 <input
                   type="text"
-                  placeholder="Search by cuisine or name..."
+                  placeholder="Search by food name"
                   value={searchTerm}
                   onChange={(e) => setSearchTerm(e.target.value)}
                   className="w-full pl-10 pr-4 py-3 border-2 border-gray-200 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-colors"
